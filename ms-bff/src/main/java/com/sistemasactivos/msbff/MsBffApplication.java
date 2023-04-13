404800b2acc94aa5d686911c9ea292e0164a210f
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-13 11:37:35 -0300

    project init

---
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/MsBffApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.msbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBffApplication.class, args);
+    }
+
+}
