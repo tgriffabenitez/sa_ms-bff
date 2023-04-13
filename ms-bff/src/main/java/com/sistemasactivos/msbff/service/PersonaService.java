3d15685b37c643e3b965ce6f7d24d8fe0ae5ee7f
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-13 13:20:58 -0300

    Elimine librerias que no utilizaba

--- ms-bff/src/main/java/com/sistemasactivos/msbff/service/PersonaService.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/service/PersonaService.java
@@ -4,8 +4,6 @@ import com.sistemasactivos.msbff.model.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
