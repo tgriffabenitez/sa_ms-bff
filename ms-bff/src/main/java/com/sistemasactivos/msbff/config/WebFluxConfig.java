9fc1061e317b6b95874805cf47b91e34bfc9fae9
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-13 19:01:16 -0300

    Cree un metodo webCliente que recibe como parametro la url del microservicio al que apunta, de esta forma evito repetir codigo

--- ms-bff/src/main/java/com/sistemasactivos/msbff/config/WebFluxConfig.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/config/WebFluxConfig.java
@@ -5,6 +5,7 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -23,8 +24,18 @@ public class WebFluxConfig implements WebFluxConfigurer {
     Logger logger = LoggerFactory.getLogger(WebFluxConfig.class);
 
     @Bean
-    public WebClient getWebClient()
-    {
+    @Qualifier("webClientPersona")
+    public WebClient getWebClientPersona() {
+        return createWebClient("http://localhost:8081/api/v1");
+    }
+
+    @Bean
+    @Qualifier("webClientCategoria")
+    public WebClient getWebClientCategoria() {
+        return createWebClient("http://localhost:8083/api/v1");
+    }
+
+    private WebClient createWebClient(String baseUrl) {
         HttpClient httpClient = HttpClient.create()
                 .tcpConfiguration(client ->
                         client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
@@ -35,10 +46,9 @@ public class WebFluxConfig implements WebFluxConfigurer {
         ClientHttpConnector connector = new ReactorClientHttpConnector(httpClient.wiretap(true));
 
         return WebClient.builder()
-                .baseUrl("http://localhost:8081/api/v1") //solo el dominio.
+                .baseUrl(baseUrl)
                 .clientConnector(connector)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
-
 }
