0404a83126c39b05f2af9b9864dea3679fb50aa9
tgriffabenitez <tgriffabenitez@gmail.com>
2023-04-14 09:14:13 -0300

    Le agregue las credenciales de acceso a los ms. Cada ms tiene distintas credenciales

--- ms-bff/src/main/java/com/sistemasactivos/msbff/config/WebFluxConfig.java
+++ ms-bff/src/main/java/com/sistemasactivos/msbff/config/WebFluxConfig.java
@@ -27,20 +27,20 @@ public class WebFluxConfig implements WebFluxConfigurer {
     @Qualifier("webClientPersona") // Le pongo un nombre para poder inyectarlo en el servicio
     public WebClient getWebClientPersona() {
         // url del microservicio persona
-        return createWebClient("http://localhost:8081/api/v1");
+        return createWebClient("http://localhost:8081/api/v1", "admin1", "123");
     }
 
     @Bean
     @Qualifier("webClientCategoria") // Le pongo un nombre para poder inyectarlo en el servicio
     public WebClient getWebClientCategoria() {
         // url del microservicio categoria
-        return createWebClient("http://localhost:8083/api/v1");
+        return createWebClient("http://localhost:8083/api/v1", "admin2", "321");
     }
 
     /**
      * Crea un cliente web para consumir los microservicios
      */
-    private WebClient createWebClient(String baseUrl) {
+    private WebClient createWebClient(String baseUrl, String username, String password) {
         HttpClient httpClient = HttpClient.create()
                 .tcpConfiguration(client ->
                         client.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 10000)
@@ -53,6 +53,7 @@ public class WebFluxConfig implements WebFluxConfigurer {
         return WebClient.builder()
                 .baseUrl(baseUrl) // url del microservicio a usar
                 .clientConnector(connector)
+                .defaultHeaders(httpHeaders -> httpHeaders.setBasicAuth(username, password)) // credenciales de acceso a la api
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
     }
